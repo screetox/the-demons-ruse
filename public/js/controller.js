d5727b1f8668eb45c80bb4eaa4e5be083df6ebcb
David <80164220+screetox@users.noreply.github.com>
2023-10-21 06:15:03 +0200

    added animation between sections

--- public/js/controller.js
+++ public/js/controller.js
@@ -17,18 +17,23 @@ function sendTestMessage(command = testInput.value) {
 // Send updated list of users
 function sendUserList() {
     const userList = [
-        {name: 'Markus', points: 19},
-        {name: 'Linus', points: 17},
-        {name: 'Maxi', points: 15},
-        {name: 'Stefan', points: 13},
-        {name: 'Julie', points: 11},
         {name: 'Elisa', points: 9},
         {name: 'Enrico', points: 7},
         {name: 'Josie', points: 5},
+        {name: 'Julie', points: 11},
+        {name: 'Linus', points: 17},
+        {name: 'Markus', points: 19},
+        {name: 'Maxi', points: 15},
+        {name: 'Stefan', points: 13},
         {name: '9', points: 3},
         {name: '10', points: 1}
     ];
     sendCommand('update-user-list', userList);
+}
+
+// Send updated list of users
+function updateUserPoints() {
+    sendCommand('update-user-points', {username: 'maxi', amount: 14});
 }
 
 // Sends commands from the controller to the display through the server
--- public/js/display.js
+++ public/js/display.js
@@ -1,10 +1,9 @@
 const socket = io();
 const title = document.getElementById('title');
 const points = document.getElementById('points');
+const sections = document.querySelectorAll(`[id^='section']`);
 const userPointsRow1 = document.getElementById('user-points-row1');
 const userPointsRow2 = document.getElementById('user-points-row2');
-
-const userList = [];
 
 // Login
 socket.emit('start-display');
@@ -15,48 +14,84 @@ socket.on('welcomeMessage', (message) => {
 });
 
 // Update list of users in case of new or deleted users
-socket.on('update-user-list', (newUserList) => {
-    userList.length = 0;
+socket.on('update-user-list', (receivedList) => {
     userPointsRow1.innerHTML = '';
     userPointsRow2.innerHTML = '';
-    if (newUserList.length > 3) {
-        for (let i = 0; i < Math.trunc((newUserList.length+1)/2); i++) {
-            userPointsRow1.innerHTML += addUserPoints(newUserList[i]);
+    const userList = [];
+    for (let i = 0; i < receivedList.length; i++) {
+        if (receivedList[i].points > 0) {
+            userList.push(receivedList[i]);
         }
-        for (let i = Math.trunc((newUserList.length+1)/2); i < newUserList.length; i++) {
-            userPointsRow2.innerHTML += addUserPoints(newUserList[i]);
+    }
+    if (userList.length > 3) {
+        for (let i = 0; i < Math.trunc((userList.length+1)/2); i++) {
+            userPointsRow1.innerHTML += addUserDiv(userList[i]);
+        }
+        for (let i = Math.trunc((userList.length+1)/2); i < userList.length; i++) {
+            userPointsRow2.innerHTML += addUserDiv(userList[i]);
         }
     } else {
-        for (let i = 0; i < newUserList.length; i++) {
-            userPointsRow1.innerHTML += addUserPoints(newUserList[i]);
+        for (let i = 0; i < userList.length; i++) {
+            userPointsRow1.innerHTML += addUserDiv(userList[i]);
         }
     }
 });
 
-function addUserPoints(user) {
-    userList.push({name: user.name, points: user.points});
+function addUserDiv(user) {
     return `
     <div class="flex column g16 m32">
         <img src="/img/testimgs/${user.name.toLowerCase()}.jpg" class="player-image">
         <h3>${user.name}</h3>
-        <h3><span style="position: relative; top: -4px;">${user.points}</span> &#10022;</h3>
+        <h3><span id="${user.name.toLowerCase()}-points" style="position: relative; top: -4px;">${user.points}</span> &#10022;</h3>
     </div>
     `;
 }
 
 // Update points of specific user
-socket.on('update-user-points', (user, amount) => {
-
-});
-
-// Switch to points  overwiev
-socket.on('show-points', () => {
-    title.style.display = 'none';
-    points.style.display = 'flex';
+socket.on('update-user-points', ({username, amount}) => {
+    const pointNr = document.getElementById(`${username}-points`);
+    if (pointNr) {
+        pointNr.innerHTML = `${amount}`;
+    }
 });
 
 // Switch to title view
 socket.on('show-title', () => {
-    title.style.display = 'flex';
-    points.style.display = 'none';
+    fadeOutAll();
+    setTimeout(changeSectionOrder, 1000, 'title');
+    setTimeout(fadeInAll, 1100);
 });
+
+// Switch to points  overwiev
+socket.on('show-points', () => {
+    fadeOutAll();
+    setTimeout(changeSectionOrder, 1000, 'points');
+    setTimeout(fadeInAll, 1100);
+});
+
+// Change section order
+function changeSectionOrder(newTopSection) {
+    let counter = 1;
+    for (let i = 0; i < sections.length; i++) {
+        if (sections[i].id === `section-${newTopSection}`) {
+            sections[i].style.order = 0;
+        } else {
+            sections[i].style.order = counter;
+            counter += 1;
+        }
+    }
+}
+
+// Fade in all sections
+function fadeInAll() {
+    for (let i = 0; i < sections.length; i++) {
+        sections[i].classList.remove('opacity0animation');
+    }
+};
+
+// Fade out all sections
+function fadeOutAll() {
+    for (let i = 0; i < sections.length; i++) {
+        sections[i].classList.add('opacity0animation');
+    }
+};
